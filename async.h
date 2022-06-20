b76d8ddb7436d477862c5d48c0f230dfce0658c5
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2022-06-20 16:11:39 +0300

    part2 and report add

--- async.h
+++ async.h
@@ -10,16 +10,65 @@
 #include <cstdlib>
 #include <string>
 #include <chrono>
+#include <sstream>
+
+std::mutex m;
+static bool exit_status;
 
 long sum_calc(const std::vector<long> &v, const int tag) {
-    // time
     long res = 0;
     for (auto &i: v) {
         res += i;
     }
-//    std::cout << "RESULT FOR THREAD" << tag << " = " << res << std::endl;
     return res;
-    // time
+}
+
+void generator(std::vector<std::vector<long>> &v, const int tag) {
+    while (!exit_status) {
+        m.lock(); // lock mutex
+
+        long random_num = random();
+        long ms = 1000 + random() % (5000 - 1000 + 1);
+
+        std::ostringstream oss;
+        oss << "Thread" << tag << " returns random number = " << random_num << " and sleeps for " << ms
+            << " milliseconds";
+        std::string str = oss.str();
+
+        std::cout << str << std::endl;
+
+        std::vector<long> res = {tag, random_num, ms};
+        v.push_back(res);
+
+        m.unlock(); // unlock mutex
+        std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+    }
+}
+
+void processor() {
+    exit_status = false;
+
+    int generators_num = 5;
+//    std::cout << "Please input generators amount: ";
+//    std::cin >> generators_num;
+
+    std::vector<std::thread> threads_generators;
+    threads_generators.resize(generators_num);
+    std::vector<std::vector<long>> random_res;
+
+    for (int i = 0; i < generators_num; i++) {
+        threads_generators[i] = std::thread(&generator, std::ref(random_res), i);
+    }
+
+    std::cin.get();
+    exit_status = true;
+
+    for (auto &i: threads_generators) {
+        i.join();
+    }
+
+    std::cout << "All threads has been finished" << std::endl;
+    std::cin.get();
 }
 
 #endif //OOP_LAB4_ASYNC_H
--- main.cpp
+++ main.cpp
@@ -1,15 +1,39 @@
 #include <iostream>
 #include "async.h"
 
+void part_1();
+void part_2();
+
 std::vector<long> async_vector;
 std::vector<std::future<long>> res_vector;
-const int VECTOR_SIZE = 100000000;
+int VECTOR_SIZE = 100000000;
 int parts_amount = 0;
 int l_policy_option = 0;
 
 using namespace std::chrono;
 
 int main() {
+    int option = 2;
+    std::cout << "1. First part, time measurement" << std::endl;
+    std::cout << "2. Second part, processor-generator" << std::endl;
+    std::cout << '\n';
+
+    switch (option) {
+        case 1:
+            part_1();
+            break;
+        case 2:
+            part_2();
+            break;
+        default:
+            break;
+    }
+    return 0;
+}
+
+void part_1() {
+    std::cout << "Enter array size: ";
+    std::cin >> VECTOR_SIZE;
 
     std::launch l_policy;
 
@@ -23,7 +47,7 @@ int main() {
     else if (l_policy_option == 2) l_policy = std::launch::async;
     else {
         std::cout << "Wrong option, terminating..." << std::endl;
-        return 0;
+        exit(1);
     }
 
     async_vector.resize(VECTOR_SIZE);
@@ -33,35 +57,39 @@ int main() {
 
     std::vector<std::vector<long>> parts;
     parts.resize(parts_amount);
+
     int one_part_amount = VECTOR_SIZE / parts_amount;
     for (int i = 0, j = 0; i < async_vector.size(); i++) {
         parts[j].push_back(async_vector[i]);
         if (i == one_part_amount) j++;
     }
 
+    // async
     for (int i = 0; i < parts.size(); i++) {
         res_vector.push_back(std::async(l_policy, sum_calc, parts[i], i));
     }
 
-
-
     long res_sum = 0;
+
     // start
     auto start = steady_clock::now();
+
     for (auto &i: res_vector) {
         res_sum += i.get();
     }
-//    for (auto &i: res_vector) {
-//        i.wait();
-//    }
+
     auto stop = steady_clock::now();
     // stop
 
-    auto time_res = duration_cast<std::chrono::milliseconds>(stop - start);
 
+    auto time_res = duration_cast<std::chrono::milliseconds>(stop - start);
     auto ms = time_res.count();
 
     std::cout << "Time: " << ms << std::endl;
     std::cout << "Sum result: " << res_sum << std::endl;
-    return 0;
+}
+
+void part_2() {
+    std::thread th_proc(processor);
+    th_proc.join();
 }
